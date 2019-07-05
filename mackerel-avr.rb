5ec6698e442e871ae284c570ee0e48a5124650c3
pyama86 <pyama@pepabo.com>
2019-07-05 15:42:12 +0900

    add mtbf and mttr

--- mackerel-avr.rb
+++ mackerel-avr.rb
@@ -3,6 +3,9 @@ require 'date'
 require 'json'
 require 'yaml'
 
+DAY_OF_MONTH = 30
+DAY_OF_WEEK = 7
+
 def conf
   @_conf ||= YAML.load_file(ENV['MACKERE_AVR_CONF'] || './avr.yml')
   @_conf
@@ -12,9 +15,9 @@ def start_day(period)
   d = Date.today
   case period
   when "month"
-    (d - 30).to_time.to_i
+    (d - DAY_OF_MONTH).to_time.to_i
   when "week"
-    (d - 7).to_time.to_i
+    (d - DAY_OF_WEEK).to_time.to_i
   else
     raise "can't support period #{period}"
   end
@@ -23,9 +26,9 @@ end
 def period_to_sec(period)
   case period
   when "month"
-    86400 * 31
+    86400 * DAY_OF_MONTH
   when "week"
-    86400 * 7
+    86400 * DAY_OF_WEEK
   else
     raise "can't support period #{period}"
   end
@@ -46,6 +49,9 @@ Mackerel.configure do |config|
   config.api_key = ENV['MACKEREL_APIKEY']
 end
 
+open_list = []
+close_list = []
+
 conf.each do|monitor_name,t|
   service_name = t['service_name']
   period = t['period']
@@ -56,22 +62,53 @@ conf.each do|monitor_name,t|
   loop do
     query_param.merge!({nextId: lastId}) if lastId
     Mackerel.alerts(lastId ? query_param.merge({nextId: lastId}) : query_param).each do |a|
+      # 処理開始日より古いレコードに到達したら処理終了
       if a.openedAt < start_day(period)
         done = true
         break
       end
 
       if a.monitorId == monitor_id(monitor_name)
-        result += (a.closedAt ? a.closedAt : start_day(period)) - a.openedAt
+        open_list << a.openedAt
+        close_list << a.closedAt
       end
       lastId = a.id
     end
     break if done
   end
 
-  Mackerel.create_service_tsdb(service_name, [{
-    name: "availability_ratio.#{period}.#{monitor_name}",
-    time: Time.now.to_i,
-    value: result == 0 ? 100 : (period_to_sec(period) - result).to_f / period_to_sec(period) * 100
-  }])
+  downtime = 0
+  atr = 0
+  last_close_time = 0
+
+  open_list.size.times do |n|
+    downtime += (close_list[n] ? close_list[n] : start_day(period)) - open_list[n]
+    if close_list[n]
+      atr = close_list[n] - open_list[n]
+
+      last_close_time
+      last_close_time = close_list[n]
+    end
+  end
+
+  mtbf = (period_to_sec(period) - downtime).to_f  / open_list.size / 60 / 60
+  mttr = (downtime / open_list.size).to_f / 60 / 60
+  availability_ratio = (period_to_sec(period) - downtime).to_f / period_to_sec(period) * 100
+  Mackerel.create_service_tsdb(service_name, [
+    {
+      name: "availability_ratio.#{period}.#{monitor_name}",
+      time: Time.now.to_i,
+      value: availability_ratio
+    },
+    {
+      name: "mtbf_hour.#{period}.#{monitor_name}",
+      time: Time.now.to_i,
+      value: mtbf
+    },
+    {
+      name: "mttr_hour.#{period}.#{monitor_name}",
+      time: Time.now.to_i,
+      value: mttr
+    }
+  ])
 end
